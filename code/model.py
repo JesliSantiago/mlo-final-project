f956f9b9c7f4cadfe2811408c9fa56c20e34f715
JesliSantiago <jjdsantiago3@gmail.com>
2023-09-10 14:17:45 +0800

    added test metrcs to sweep

--- code/model.py
+++ code/model.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 import nltk
 import string
@@ -16,6 +17,12 @@ class poem_classifier_model():
         self.df_train = None
         self.df_test = None
         self.trained_model = None
+        self.tokenizer = None
+        self.max_len = None
+        self.le = None
+        self.otpt_act = tf.nn.softmax
+        self.ls = tf.keras.losses.SparseCategoricalCrossentropy()
+        self.model = None
 
     def load_data(self):
         self.df_train = pd.read_csv('https://raw.githubusercontent.com/brandynewanek/data/main/Poem_classification%20-%20train_data.csv')
@@ -42,39 +49,48 @@ class poem_classifier_model():
         self.df_train['Poem'] = self.df_train['Poem'].replace (pntn, ' ')
 
     def _tokenize_encode(self, inpt_tr, inpt_ts, otpt_tr, otpt_ts, num_words=500):
-        tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=num_words,
+        self.tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=num_words,
                                                           filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~',
                                                           oov_token='<oov>')
-        tokenizer.fit_on_texts(inpt_tr)
-        inpt_tr = tokenizer.texts_to_sequences(inpt_tr)
-        inpt_ts = tokenizer.texts_to_sequences(inpt_ts)
-        max_len = max([len(s) for s in inpt_tr])
+        self.tokenizer.fit_on_texts(inpt_tr)
+        inpt_tr = self.tokenizer.texts_to_sequences(inpt_tr)
+        inpt_ts = self.tokenizer.texts_to_sequences(inpt_ts)
+        self.max_len = max([len(s) for s in inpt_tr])
         inpt_tr = tf.keras.utils.pad_sequences(inpt_tr, padding='pre',
-                                               maxlen=max_len)
+                                               maxlen=self.max_len)
         inpt_ts = tf.keras.utils.pad_sequences(inpt_ts, padding='pre',
-                                               maxlen=max_len)
-        le = LabelEncoder().fit(otpt_tr)
-        otpt_tr = le.transform(otpt_tr)
-        otpt_ts = le.transform(otpt_ts)
-        return inpt_tr, inpt_ts, otpt_tr, otpt_ts, max_len
+                                               maxlen=self.max_len)
+        self.le = LabelEncoder().fit(otpt_tr)
+        otpt_tr = self.le.transform(otpt_tr)
+        otpt_ts = self.le.transform(otpt_ts)
+        return inpt_tr, inpt_ts, otpt_tr, otpt_ts
 
     def train(self, num_words=500, embd_dim=512, lr=.001, epochs=16, optimizer='adam'):
         inpt_tr, inpt_ts, otpt_tr, otpt_ts = train_test_split(self.df_train['Poem'], self.df_train['Genre'], 
                                                               test_size=.2)
-        inpt_tr, inpt_ts, otpt_tr, otpt_ts, max_len = self._tokenize_encode(inpt_tr, inpt_ts, otpt_tr, otpt_ts)
-        otpt_act = tf.nn.softmax
+        inpt_tr, inpt_ts, otpt_tr, otpt_ts = self._tokenize_encode(inpt_tr, inpt_ts, otpt_tr, otpt_ts)
         if optimizer == 'adam':
             opt = tf.keras.optimizers.Adam(learning_rate=lr)
         elif optimizer == 'sgd':
             opt = tf.keras.optimizers.SGD(learning_rate=lr)
-        ls = tf.keras.losses.SparseCategoricalCrossentropy()
-        model = tf.keras.models.Sequential([tf.keras.layers.Embedding(num_words*3, embd_dim,
-                                                                    input_length=max_len),
+        self.model = tf.keras.models.Sequential([tf.keras.layers.Embedding(num_words*3, embd_dim, 
+                                                                      input_length=self.max_len),
                                             tf.keras.layers.SpatialDropout1D(.45),
                                             tf.keras.layers.GlobalAveragePooling1D(),
                                             tf.keras.layers.Flatten(),
-                                            tf.keras.layers.Dense(4, activation=otpt_act)
+                                            tf.keras.layers.Dense(4, activation=self.otpt_act)
                                             ])
-        model.compile(loss= ls, optimizer=opt, metrics=['acc'])
-        self.trained_model = model.fit(inpt_tr, otpt_tr, epochs=epochs, validation_data=(inpt_ts, otpt_ts))
+        self.model.compile(loss=self.ls, optimizer=opt, metrics=['acc'])
+        self.trained_model = self.model.fit(inpt_tr, otpt_tr, epochs=epochs, validation_data=(inpt_ts, otpt_ts))
+
+    def test(self):
+        if self.trained_model is not None:
+            input = self.tokenizer.texts_to_sequences(self.df_test['Poem'])
+            input = tf.keras.utils.pad_sequences(input, padding='pre', 
+                                                 maxlen=self.max_len)
+            output = self.le.transform(self.df_test['Genre'])
+            results = self.model.evaluate(input, output)
+            return results
+        else:
+            print("No trained model.")
 
